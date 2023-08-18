fea8562b43672908d0122465de3800632a243e26
Avani Vaish <77274574+avanivaish@users.noreply.github.com>
2023-08-18 13:44:27 +0530

    Add files via upload

---
+++ 75 Days DSA/day9.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    //int n = 6;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        int j = i + 1;
+        int k = n - 1;
+
+        while (j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum > 0) {
+                k--;
+            } else if (sum < 0) {
+                j++;
+            } else {
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+                while (j < k && nums[k] == nums[k + 1])
+                    k--;
+            }
+        }
+    }
+
+    for (const auto &triplet : ans) {
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
