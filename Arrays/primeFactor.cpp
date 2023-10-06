4212a78c6de2bdba2694bb54457b5376bf585bdb
Avani Vaish <77274574+avanivaish@users.noreply.github.com>
2023-10-06 21:25:46 +0530

    Add files via upload

---
+++ Arrays/primeFactor.cpp
@@ -0,0 +1,36 @@
+//using Sieve
+#include<iostream>
+using namespace std;
+void primeFactors(int n);
+int main(){
+    int n = 42;
+    primeFactors(n);
+    int n1 = 50;
+    primeFactors(n1);
+    return 0;
+}
+
+void primeFactors(int n){
+    int spf[100]={0};
+    for(int i=2;i<=n;i++){
+        spf[i]=i;
+    }
+
+    for(int i=2;i<=n;i++){
+        if(spf[i]==i){
+            for(int j=i*i;j<=n;j+=i){
+                if(spf[j]==j){
+                    spf[j]=i;
+                }
+            }
+        }
+    }
+
+    while(n!=1){
+        cout<<spf[n]<<" ";
+        n=n/spf[n];
+    }
+
+    cout<<endl;
+
+}
