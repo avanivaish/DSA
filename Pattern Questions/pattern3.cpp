49e2769b23dd908d7add5ae775ded29505e338cc
Avani Vaish <77274574+avanivaish@users.noreply.github.com>
2023-10-13 16:55:56 +0530

    Add files via upload

---
+++ Pattern Questions/pattern3.cpp
@@ -0,0 +1,16 @@
+/*
+321
+321
+321
+*/
+#include<iostream>
+using namespace std;
+int main() {
+    int n = 3;
+    for(int i=1;i<=n;i++){
+        for(int j=n; j>=1; j--){
+            cout<<j;
+        }cout<<endl;
+    }
+    return 0;
+}
